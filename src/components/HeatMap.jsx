a7be51afae51edf599c3f94ff5f81487aa91a564
Juan Nicolás Hernández Chávez <juann-hernandezc@unilibre.edu.co>
2022-08-19 15:01:50 -0500

    Add new functionality in map to show more information about place select

--- src/components/HeatMap.jsx
+++ src/components/HeatMap.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-import { Map, TileLayer } from "react-leaflet";
+import { Map, TileLayer, Marker, Popup } from "react-leaflet";
 import HeatmapLayer from "react-leaflet-heatmap-layer";
 import "./styles/HeatMap.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faClose } from "@fortawesome/free-solid-svg-icons";
+import { cityData } from "../helpers/colombiaData";
 
 export const HeatMap = (locations) => {
   const position = [4.6126, -74.0705];
@@ -11,6 +12,25 @@ export const HeatMap = (locations) => {
     const popUp = document.getElementsByClassName("pop-up__heatmap");
     popUp[0].style.bottom = "125%";
   };
+
+  const calcLocations = () => {
+    const repeat = {};
+
+    locations.data.forEach(function ({ city }) {
+      repeat[city] = (repeat[city] || 0) + 1;
+    });
+
+    const arrayRepeat = Object.entries(repeat);
+    const mapear = arrayRepeat.map((item) => {
+      return {
+        ciudad: item[0],
+        cantidad: item[1],
+      };
+    });
+    return mapear;
+  };
+
+  calcLocations();
   return (
     <>
       <div className="container-close">
@@ -34,6 +54,23 @@ export const HeatMap = (locations) => {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
+        {calcLocations().map((item, index) => {
+          let city = [];
+          cityData.forEach((loc) => {
+            if (item.ciudad === loc.city) {
+              city = [loc.lat, loc.lng];
+            }
+          });
+
+          return (
+            <Marker position={city} key={index}>
+              <Popup>
+                <p><b>Ciudad: </b> {item.ciudad}</p>
+                <p> <b>Cantidad Tweets: </b> {item.cantidad}</p>
+              </Popup>
+            </Marker>
+          );
+        })}
       </Map>
     </>
   );
